0c53fc2a1ac9826d46863510e6d1a9e4b3175ab3
Jake Mander <Jake@chilli-is.co.uk>
2021-05-25 14:49:10 +0100

    Completed State and Getters.

--- mastering-vuex/src/store/index.js
+++ mastering-vuex/src/store/index.js
@@ -2,9 +2,37 @@ import { createStore } from "vuex";
 
 export default createStore({
   state: {
-    user: { id: 'abc123', name: 'Jake Mander' }
+    user: { id: 'abc123', name: 'Jake Mander' },
+    categories: ['sustainability', 'education', 'food', 'community'],
+    todos: [
+      { id: 1, text: 'Clean The Dishes', done: false },
+      { id: 2, text: 'Walk The Pooch', done: false },
+      { id: 3, text: 'Study Time', done: true },
+      { id: 4, text: 'Empty The Bins', done: false }
+    ],
+    events: [
+      { id: 1, title: 'Beach Clean Up', organiser: 'David Hasslehof'},
+      { id: 2, title: 'Cat Adoption', organiser: 'The Crazy Cat Lady'},
+      { id: 3, title: 'Charity Fun Run', organiser: 'Bobby Big Stride'},
+      { id: 4, title: 'Chairty Bingo', organiser: 'Dorris Eiezdown'}
+    ]
   },
   mutations: {},
   actions: {},
+  getters: {
+    categoriesLength: state => {
+      return state.categories.length
+    },
+    doneTodos: state => {
+      return state.todos.filter(todo => todo.done);
+    },
+    activeTodoCount: (state, getters) => {
+      return state.todos.length - getters.doneTodos.length;
+    },
+
+    getEventByID: state => id => {
+      return state.events.find(event => event.id === id);
+    }
+  },
   modules: {},
 });
--- mastering-vuex/src/views/EventCreate.vue
+++ mastering-vuex/src/views/EventCreate.vue
@@ -1,13 +1,56 @@
 <template>
-  <h1>Create Event</h1>
+    <div>
+        <h1>Create Event {{ user.name}} </h1>
+        <p>This event was created by {{ user.name}}</p>
+        <p class="tags">Tags: </p>
+        <ul class="tags categories">
+            <li v-for="category in categories" :key="category"> {{ category }}</li>
+        </ul>
+        <p>There Are {{ categoriesLength }} Categories</p>
+        <p class="details">ID: {{ user.id }}</p>
+        <p>{{ localScopeMethodOne }}</p>
+
+        <h2>Event Of The Day</h2>
+        <p> Event: {{ getEventByID(2).title }}, Organised By: {{ getEventByID(2).organiser}}</p>
+
+        <h2>Test ToDo's</h2>
+        <p>There are currently {{ activeTodoCount }} Incomplete Todo's</p>
+
+    </div>
 </template>
 
 <script>
-export default {
+import { mapState, mapGetters } from 'vuex';
 
+export default {
+    computed: {
+        ...mapState(['user', 'categories']),
+        ...mapGetters(['getEventByID', 'categoriesLength', 'activeTodoCount']),
+        localScopeMethodOne() {
+            return "This Is A Test";
+        }
+    }
 }
 </script>
 
 <style>
+    .details {
+        font-style: italic;
+        font-size: .75rem;
+    }
 
+    .categories {
+        text-decoration: none;
+        list-style: none;
+        padding: 0px;
+    }
+
+    .categories li, .tags {
+        display: inline;
+        
+    }
+
+    .categories li::after {
+        content: ", "
+    }
 </style>
